1ae29e5990e171f34501096e621c8785e90e718a
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-08 17:38:26 -0500

    Adding update route and its logic

--- routes/todos.js
+++ routes/todos.js
@@ -33,5 +33,15 @@ router.get("/:todoId", function(req, res) {
     })
 })
 
+router.put("/:todoId", function(req, res) {
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true}) //responds by default with old data, unless specified with {new: true}
+    .then(function(updateTodo) {
+        res.json(updateTodo);
+    })
+    .catch(function(err) {
+        res.send(err);
+    })
+})
+
 
 module.exports = router; //exports router
