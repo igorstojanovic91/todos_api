328eb0449d093d8d390a7537d65e7f4b9ebec992
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-08 16:22:45 -0500

    Installing mongoose

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require('express'),
-    app     = express();
+var express     = require('express'),
+    app         = express(),
+    mongoose    = require('mongoose');
 
 app.get("/", function(req, res) {
-    res.send("I got here");
+    res.send("I got here"); //when here an object is plaed it is converted to JSON; use res.json({message: "Test"})
 })
 
+
+//Starting up server
 var port = process.env.PORT || 3000
 app.listen(port, process.env.IP, function() {
     console.log("app started");
