389861f025973ee29893fb21904217724d34c262
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-08 15:55:21 -0500

    Adding get / route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     app     = express();
 
-
+app.get("/", function(req, res) {
+    res.send("I got here");
+})
 
 var port = process.env.PORT || 3000
 app.listen(port, process.env.IP, function() {
